84c049a57f6db6df0bcdf9a153a1feaf484e2cc4
Gilberto <gilberto.acevedo.xmarts@gmail.com>
2022-02-02 13:59:04 -0600

    [FIX] v15

--- reporte_factura/models/models.py
+++ reporte_factura/models/models.py
@@ -9,7 +9,6 @@ class AddFieldAmountText(models.Model):
 
 	cam_monto_letra = fields.Char(compute='_get_amount_to_text', string='Monto en letra', readonly=True, help='Amount of the invoice in letter')
 
-	@api.one
 	@api.depends('amount_total')
 	def _get_amount_to_text(self):
 		self.cam_monto_letra = monto_letra.get_amount_to_text(self, self.amount_total, self.currency_id.name)
